b1c9510e1c761f73bd519f1fa0508108e906466c
Jade Allen <jadeallenx@outlook.com>
2022-11-08 14:14:37 -0600

    Handle oracle packaging differently
    Add fpm
    
    Make poc_entropy naming consistent
    
    sudo make me a sandwich
    
    test w/release objs, attempt release rebuild pre-pkg
    
    cargo clippy in release mode
    
    More package renames for consistency
    
    reorder clippy args after brief enlightenment
    
    File cleanup; add service subcommand

--- poc_entropy/src/error.rs
+++ poc_entropy/src/error.rs
@@ -14,47 +14,38 @@ pub enum Error {
     Encode(#[from] EncodeError),
     #[error("dencode error")]
     Decode(#[from] DecodeError),
-    #[error("grpc {}", .0.message())]
-    Grpc(#[from] tonic::Status),
     #[error("service error")]
     Service(#[from] helium_proto::services::Error),
+    #[error("http server error")]
+    Server(#[from] hyper::Error),
     #[error("store error")]
     Store(#[from] file_store::Error),
+    #[error("mpsc channel error")]
+    Channel,
     #[error("not found")]
     NotFound(String),
-    #[error("crypto error")]
-    Crypto(#[from] helium_crypto::Error),
 }
 
 #[derive(Error, Debug)]
 pub enum DecodeError {
     #[error("prost error")]
     Prost(#[from] helium_proto::DecodeError),
+    #[error("bs58 error")]
+    Bs58(#[from] bs58::decode::Error),
     #[error("uri error")]
     Uri(#[from] http::uri::InvalidUri),
     #[error("socket addr error")]
     SocketAddr(#[from] std::net::AddrParseError),
+    #[error("jsonrpc error")]
+    JsonRpc(#[from] jsonrpsee::core::Error),
 }
 
 #[derive(Error, Debug)]
 pub enum EncodeError {
     #[error("prost error")]
     Prost(#[from] helium_proto::EncodeError),
-}
-
-impl Error {
-    pub fn not_found<E: ToString>(msg: E) -> Self {
-        Self::NotFound(msg.to_string())
-    }
-}
-
-impl From<Error> for tonic::Status {
-    fn from(v: Error) -> Self {
-        match v {
-            Error::NotFound(msg) => tonic::Status::not_found(msg),
-            _other => tonic::Status::internal("internal error"),
-        }
-    }
+    #[error("json error")]
+    Json(#[from] serde_json::Error),
 }
 
 impl From<Error> for (http::StatusCode, String) {
@@ -75,7 +66,20 @@ macro_rules! from_err {
 
 // Encode Errors
 from_err!(EncodeError, prost::EncodeError);
+from_err!(EncodeError, serde_json::Error);
 
 // Decode Errors
 from_err!(DecodeError, http::uri::InvalidUri);
 from_err!(DecodeError, prost::DecodeError);
+from_err!(DecodeError, jsonrpsee::core::Error);
+from_err!(DecodeError, bs58::decode::Error);
+
+impl Error {
+    pub fn not_found<E: ToString>(msg: E) -> Self {
+        Self::NotFound(msg.to_string())
+    }
+
+    pub fn channel() -> Error {
+        Error::Channel
+    }
+}
