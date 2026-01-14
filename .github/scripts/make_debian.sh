70a108a6e448612623222f7b8d5665dfad006b24
Michael Jeffrey <michaeldjeffrey@gmail.com>
2026-01-14 10:55:13 -0700

    Push to the correct packagecloud endpoint
    This is what we get when we find/replace and get bored...

--- .github/scripts/make_debian.sh
+++ .github/scripts/make_debian.sh
@@ -129,5 +129,5 @@ do
     curl -u "${PACKAGECLOUD_API_KEY}:" \
          -F "package[distro_version_id]=210" \
          -F "package[package_file]=@$deb" \
-         https://packagecloud.io/api/v1/repos/helium/oracles-iot/packages.json
+         https://packagecloud.io/api/v1/repos/helium/oracles/packages.json
 done
